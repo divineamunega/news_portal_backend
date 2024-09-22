ba43ce0459b8155d9d0d6f58ba3e15a528a35da6
divineamunega <divineamunega@gmail.com>
2024-09-22 23:03:00 +0100

    Hello

--- dist/server.js
+++ dist/server.js
@@ -3,10 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 3000;
-const express_1 = __importDefault(require("express"));
-const server = (0, express_1.default)();
-server.listen(PORT, () => {
+app_1.default.listen(PORT, () => {
     var _a;
     console.log(`Server running on port ${PORT} in ${(_a = process.env.ENVIROMENT) === null || _a === void 0 ? void 0 : _a.toLowerCase()}.`);
 });
